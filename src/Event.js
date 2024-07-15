0ad5e179a38cbd252d41e2d89de680bfddf673e0
aussttiinn <puia.austin@gmail.com>
2024-07-15 18:23:56 -0400

    sets random color at the moment

--- src/Event.js
+++ src/Event.js
@@ -6,15 +6,25 @@ function getOptions() {
         updatedMin: earlier.toISOString(),
         maxResults: 50,
         orderBy: 'updated',
-        singleEvents: true,
+        singleEvents: false,
         showDeleted: false
     }
+}
+
+function getRandomNumber() {
+    // Math.random() generates a random number between 0 (inclusive) and 1 (exclusive)
+    // Multiplying by 11 gives a range from 0 to 10.999...
+    // Adding 1 shifts the range to 1 to 11.999...
+    // Math.floor() rounds down to the nearest whole number, resulting in a range from 1 to 11
+    var randomNumber = Math.floor(Math.random() * 11) + 1;
+    Logger.log('Random Number: ' + randomNumber);
+    return randomNumber;
 }
 
 function getLastEditedEvent() {
     // returns https://developers.google.com/apps-script/reference/calendar/calendar-event
     var options = getOptions();
-    var calendarId = Session.getEffectiveUser().getEmail();
+    var calendarId = Session.getActiveUser().getEmail();
     var events = Calendar.Events.list(calendarId, options);
 
     if (!events.items || events.items.length === 0) {
--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,11 @@
 function onEventChange(event) {
-    // set the trigger 
     Trigger.calendarId=event["calendarId"];
     Trigger.triggerUid=event["triggerUid"];
     Trigger.authMode=event["authMode"];
-
-    main(event);
+    main();
 }
 
-function main(event) {
-    Logger.log(JSON.stringify(Trigger));
+function main() {
     lastUpdated = getLastEditedEvent();
-    Logger.log(JSON.stringify(lastUpdated));
+    lastUpdated.setColor(getRandomNumber());
 }
